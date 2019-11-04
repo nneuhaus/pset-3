d72ae7c4b492ddda953f101f9890d43c996ecd85
Nathalie Neuhaus <nneuhaus@ucvts.org>
2019-11-04 13:19:50 -0500

    what did you do nathalie

--- src/gpa.js
+++ src/gpa.js
@@ -2,7 +2,3 @@ const readlineSync = require("readline-sync");
 
 const Grade = readlineSync.question("\nEnter an letter grade: ");
 console.log(Grade)
-
-if (Grade.NaI) {
-console.log("Invalid.")
-}
